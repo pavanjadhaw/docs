8fc089d139086385e00ec0036eb6ee9c8568bbc6
Josue Montano <josuemontanoa@gmail.com>
2021-07-06 21:07:10 -0400

    Load Markdown files with the raw-loader

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx?/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "raw-loader",
+          options: {},
+        },
+      ],
+    });
+
+    return config;
+  },
+};
