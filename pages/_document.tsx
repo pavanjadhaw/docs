48da147007698a05e492b47eb82684bf67272996
Josue Montano <josuemontanoa@gmail.com>
2021-07-06 23:55:59 -0400

    Add the intercom and segment scripts

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import IntercomScripts from "./components/scripts/IntercomScripts";
+import SegmentScripts from "./components/scripts/SegmentScripts";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>MagicBell - Docs</Head>
+        <body>
+          <Main />
+          <NextScript />
+          <SegmentScripts />
+          <IntercomScripts />
+        </body>
+      </Html>
+    );
+  }
+}
