b8a9d640b630ece63d24d73fe91eaca3f00f6134
Josue Montano <josuemontanoa@gmail.com>
2021-07-06 23:50:12 -0400

    Import the material theme for highlight-js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import "../styles/material.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+
+export default MyApp;
