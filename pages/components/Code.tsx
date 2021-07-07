0bf56601f178bdda6b68678f7f01863dd7c210d3
Josue Montano <josuemontanoa@gmail.com>
2021-07-07 12:05:36 -0400

    Style improvements to the header

--- pages/components/Code.tsx
+++ pages/components/Code.tsx
@@ -1,32 +1,32 @@
-import classNames from "classnames";
-import hljs from "highlight.js/lib/core";
-import bash from "highlight.js/lib/languages/bash";
-import diff from "highlight.js/lib/languages/diff";
-import java from "highlight.js/lib/languages/java";
-import javascript from "highlight.js/lib/languages/javascript";
-import json from "highlight.js/lib/languages/json";
-import php from "highlight.js/lib/languages/php";
-import python from "highlight.js/lib/languages/python";
-import ruby from "highlight.js/lib/languages/ruby";
-import typescript from "highlight.js/lib/languages/typescript";
-import xml from "highlight.js/lib/languages/xml";
+import classNames from 'classnames';
+import hljs from 'highlight.js/lib/core';
+import bash from 'highlight.js/lib/languages/bash';
+import diff from 'highlight.js/lib/languages/diff';
+import java from 'highlight.js/lib/languages/java';
+import javascript from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+import php from 'highlight.js/lib/languages/php';
+import python from 'highlight.js/lib/languages/python';
+import ruby from 'highlight.js/lib/languages/ruby';
+import typescript from 'highlight.js/lib/languages/typescript';
+import xml from 'highlight.js/lib/languages/xml';
 // @ts-ignore
-import curl from "highlightjs-curl";
-import React from "react";
+import curl from 'highlightjs-curl';
+import React from 'react';
 
-hljs.registerLanguage("bash", bash);
-hljs.registerLanguage("curl", curl);
-hljs.registerLanguage("diff-html", diff);
-hljs.registerLanguage("java", java);
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("json", json);
-hljs.registerLanguage("node", javascript);
-hljs.registerLanguage("php", php);
-hljs.registerLanguage("python", python);
-hljs.registerLanguage("ruby", ruby);
-hljs.registerLanguage("shell", bash);
-hljs.registerLanguage("typescript", typescript);
-hljs.registerLanguage("xml", xml);
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('curl', curl);
+hljs.registerLanguage('diff-html', diff);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('node', javascript);
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('ruby', ruby);
+hljs.registerLanguage('shell', bash);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('xml', xml);
 
 export interface CodeProps {
   children: string;
@@ -36,26 +36,25 @@ export interface CodeProps {
 export default function Code({ children: code, className }: CodeProps) {
   if (!code) return null;
 
-  const language = className?.replace("language-", "") || "bash";
+  const language = className?.replace('language-', '') || 'bash';
 
   function parseCode(str: string) {
     return hljs.highlight(str, { language }).value;
   }
 
   return (
-    <code className={classNames("block my-4", className)}>
+    <code className={classNames('block my-4', className)}>
       <div
-        className="px-6 py-2 rounded-t-md uppercase text-xs font-bold"
+        className="px-6 py-3 rounded-t-md uppercase text-xs font-bold text-white"
         style={{
-          background: "#222337",
-          color: "#c792ea",
+          background: '#222337',
         }}
       >
         {language}
       </div>
       <pre
         style={{
-          background: "#31324e",
+          background: '#31324e',
           fontFamily:
             'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
         }}
