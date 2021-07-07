a63ff371a6f01c5a5df615c625fb1782510a6290
Josue Montano <josuemontanoa@gmail.com>
2021-07-07 01:17:43 -0400

    Increase padding in mobile

--- pages/components/layout/SearchBar.tsx
+++ pages/components/layout/SearchBar.tsx
@@ -13,7 +13,7 @@ export default function SearchBar() {
         </div>
         <input
           id="search-field"
-          className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+          className="block h-full w-full border-transparent py-4 md:py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
           placeholder="Search"
           type="search"
           name="search"
