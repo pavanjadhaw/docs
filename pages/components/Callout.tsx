b162c50f9bb8b276e9a4520a94fb02d3509da840
Josue Montano <josuemontanoa@gmail.com>
2021-07-07 01:33:09 -0400

    Define types oof props

--- pages/components/Callout.tsx
+++ pages/components/Callout.tsx
@@ -1,7 +1,12 @@
 import classNames from "classnames";
 import React from "react";
 
-export default function Callout({ children, className }) {
+interface Props {
+  children: JSX.Element | JSX.Element[];
+  className: string;
+}
+
+export default function Callout({ children, className }: Props) {
   return (
     <div
       className={classNames(
