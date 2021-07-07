fb059a236213e027490f3baae2f14a01f633abe4
Josue Montano <josuemontanoa@gmail.com>
2021-07-07 10:04:40 -0400

    Improve spacing

--- pages/components/layout/Content.tsx
+++ pages/components/layout/Content.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Content({ children }: Props) {
   return (
     <div className="max-w-4xl mx-auto w-0 flex-1 flex flex-col md:px-8 xl:px-0 mb-20">
-      <div className="md:pt-10 px-4 md:px-0 border-b border-gray-200">
+      <div className="md:mt-4 md:py-4 px-4 md:px-0 border-b border-gray-200">
         <SearchBar />
       </div>
       <main className="flex-1 relative overflow-y-auto focus:outline-none">
