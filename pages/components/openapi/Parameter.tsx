b76cbcc0d53cc48d7aa2b205a4ccd1cf9e9768f9
Josue Montano <josuemontanoa@gmail.com>
2021-07-08 14:41:14 -0400

    Create component to render a parameter

---
+++ pages/components/openapi/Parameter.tsx
@@ -0,0 +1,19 @@
+import { OpenAPIV3 } from 'openapi-types';
+import React from 'react';
+
+interface Props {
+  param?: OpenAPIV3.ParameterObject;
+}
+
+export default function Parameter({ param }: Props) {
+  if (!param) return null;
+  return (
+    <div className="mb-4">
+      <p className="font-mono flex items-center m-0">
+        {param.name}
+        {param.required && <span className="text-red-500 mx-4 text-xs">required</span>}
+      </p>
+      <p className="opacity-60">{param.description}</p>
+    </div>
+  );
+}
