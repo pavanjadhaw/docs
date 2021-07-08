99b676a8dbc09cdb27c246ecce9ab987f8a9dde4
Josue Montano <josuemontanoa@gmail.com>
2021-07-08 18:57:47 -0400

    Render the request body shcma

--- pages/components/openapi/Operation.tsx
+++ pages/components/openapi/Operation.tsx
@@ -2,6 +2,7 @@ import { OpenAPIV3 } from 'openapi-types';
 import React from 'react';
 import HeaderParameters from './HeaderParameters';
 import Request from './Request';
+import RequestBody from './RequestBody';
 import Responses from './Responses';
 
 interface Props {
@@ -17,8 +18,12 @@ export default function Operation({ pathKey, operation, method }: Props) {
       <div className="flex space-x-0 xl:space-x-4 flex-wrap xl:flex-nowrap">
         <main className="w-full xl:w-1/2">
           <p>{operation.description}</p>
-          {/* @ts-ignore */}
-          <HeaderParameters parameteres={operation.parameters} />
+          <HeaderParameters
+            parameteres={operation.parameters as OpenAPIV3.ParameterObject[]}
+          />
+          <RequestBody
+            requestBody={operation.requestBody as OpenAPIV3.RequestBodyObject}
+          />
         </main>
         <aside className="w-full xl:w-1/2 space-y-12">
           <Request method={method} location={pathKey} operation={operation} />
---
+++ pages/components/openapi/RequestBody.tsx
@@ -0,0 +1,42 @@
+import { OpenAPIV3 } from 'openapi-types';
+import React from 'react';
+
+interface Props {
+  requestBody?: OpenAPIV3.RequestBodyObject;
+}
+
+export default function RequestBody({ requestBody }: Props) {
+  if (!requestBody) return null;
+
+  const properties = requestBody.content?.['application/json']?.schema?.properties;
+
+  const renderProperty = (propName, prop, acc = []) => {
+    if (prop.type === 'object')
+      return (
+        <>
+          {Object.keys(prop.properties || {}).map((p) =>
+            renderProperty(p, prop.properties[p], [...acc, p]),
+          )}
+        </>
+      );
+    return (
+      <div>
+        <p className="m-0 font-mono">
+          {acc.join('.')}{' '}
+          {prop.nullable && <span className="text-red-500 mx-4 text-xs">required</span>}
+        </p>
+        <p className="m-0 opacity-60">{prop.description}</p>
+        <p className="opacity-60 capitalize">{prop.type}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="mt-8 mb-12 overflow-hidden">
+      <p className="uppercase text-sm border-b border-gray-200 pb-2">Body Parameters</p>
+      {Object.keys(properties || {}).map((key) =>
+        renderProperty(key, properties[key], [key]),
+      )}
+    </div>
+  );
+}
