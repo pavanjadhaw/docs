c2ce93f26185bd7f26f32bd1876915c864127d61
Josue Montano <josuemontanoa@gmail.com>
2021-07-08 01:29:56 -0400

    Show checkmark when the code is copied

--- pages/components/CopyButton.tsx
+++ pages/components/CopyButton.tsx
@@ -1,12 +1,24 @@
-import { ClipboardIcon } from '@heroicons/react/outline';
+import { CheckIcon, ClipboardIcon } from '@heroicons/react/outline';
 import React from 'react';
-import { useCopyToClipboard } from 'react-use';
+import { useCopyToClipboard, useToggle } from 'react-use';
 
 export default function CopyButton({ text }: { text: string }) {
-  const [state, copyToClipboard] = useCopyToClipboard();
+  const [copied, toggleCopied] = useToggle(false);
+  const [_, copyToClipboard] = useCopyToClipboard();
+
+  const handleClick = () => {
+    copyToClipboard(text);
+    toggleCopied();
+    setTimeout(toggleCopied, 1000);
+  };
+
   return (
-    <button type="button" onClick={() => copyToClipboard(text)}>
-      <ClipboardIcon className="h-4 w-4" />
+    <button type="button" onClick={handleClick}>
+      {copied ? (
+        <CheckIcon className="h-4 w-4 text-green-400" />
+      ) : (
+        <ClipboardIcon className="h-4 w-4" />
+      )}
     </button>
   );
 }
