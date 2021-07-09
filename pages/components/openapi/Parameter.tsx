5c7daadcfd58d1c9979ac31fe35bc34ef904d9cc
Josue Montano <josuemontanoa@gmail.com>
2021-07-09 15:54:15 -0400

    Add opacity

--- pages/components/openapi/Parameter.tsx
+++ pages/components/openapi/Parameter.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Parameter({ param }: Props) {
   if (!param) return null;
   return (
-    <div className="mb-4 bg-gray-100 rounded p-2">
+    <div className="mb-4 bg-gray-100 bg-opacity-75 rounded p-2">
       <p className="font-mono flex items-center m-0">
         {param.name}
         {param.required && <span className="text-red-500 mx-4 text-xs">required</span>}
