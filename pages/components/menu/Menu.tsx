92eaf5da08e3d742bcc6d19d86802ef8eaeee1f1
Josue Montano <josuemontanoa@gmail.com>
2021-07-09 15:54:05 -0400

    Format file

--- pages/components/menu/Menu.tsx
+++ pages/components/menu/Menu.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { SitemapItem } from "../../../lib/sitemap";
-import MenuItem from "./MenuItem";
+import React from 'react';
+import { SitemapItem } from '../../../lib/sitemap';
+import MenuItem from './MenuItem';
 
 interface Props {
   navigationItems: SitemapItem[];
