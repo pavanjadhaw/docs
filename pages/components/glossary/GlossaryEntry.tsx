f11ef6185a4fd53a07bddb07b0b3d49afc7c8712
Josue Montano <josuemontanoa@gmail.com>
2021-07-09 21:17:11 -0400

    Implement the glossary page

---
+++ pages/components/glossary/GlossaryEntry.tsx
@@ -0,0 +1,26 @@
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import React from 'react';
+import slugify from 'slugify';
+
+interface Props {
+  metadata?: { [key: string]: any };
+  mdxSource?: MDXRemoteSerializeResult;
+}
+
+export default function GlossaryEntry({ mdxSource, metadata = {} }: Props) {
+  return (
+    <div>
+      <h3 id={slugify(metadata.title).toLowerCase()} className="mt-16 mb-0">
+        {metadata.title}
+      </h3>
+      <p className="text-gray-500">{metadata.shortDescription}</p>
+      {mdxSource ? (
+        <article className="mdx-content">
+          <MDXRemote {...mdxSource} />
+        </article>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
