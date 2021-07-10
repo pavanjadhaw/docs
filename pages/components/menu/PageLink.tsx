9e3faf1bbb705346bda396b8a5ae67919676285e
Josue Montano <josuemontanoa@gmail.com>
2021-07-10 00:03:24 -0400

    Increase foont size oof the menu

--- pages/components/menu/PageLink.tsx
+++ pages/components/menu/PageLink.tsx
@@ -14,8 +14,8 @@ export default function PageLink({ name, to, icon }: SitemapItem) {
         className={classNames(
           isActive
             ? 'bg-white text-gray-800 font-bold'
-            : 'text-gray-600 hover:bg-white group-hover:text-gray-800',
-          'group py-3 px-6 flex items-center md:text-sm',
+            : 'text-gray-600 hover:bg-white hover:text-gray-800',
+          'group py-3 px-6 flex items-center',
         )}
       >
         {name}
