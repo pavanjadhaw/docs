cf69b7b5aeb507338e77cee97dd2002ce668a8ea
Josue Montano <josuemontanoa@gmail.com>
2021-07-09 20:18:26 -0400

    increase text contrast

--- pages/components/openapi/Parameter.tsx
+++ pages/components/openapi/Parameter.tsx
@@ -13,8 +13,8 @@ export default function Parameter({ param }: Props) {
         {param.name}
         {param.required && <span className="text-red-500 mx-4 text-xs">required</span>}
       </p>
-      <p className="opacity-60 m-0">{param.description}</p>
-      <p className="opacity-60 m-0 capitalize">
+      <p className="opacity-80 m-0">{param.description}</p>
+      <p className="opacity-80 m-0 capitalize">
         {/* @ts-ignore */}
         {param.schema?.type}
       </p>
