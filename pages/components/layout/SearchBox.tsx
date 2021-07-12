15a462ef6dd6ff93bfde2dfe34f8b48fb46767f0
Josue Montano <josuemontanoa@gmail.com>
2021-07-12 18:15:02 -0400

    Use heroicon

--- pages/components/layout/SearchBox.tsx
+++ pages/components/layout/SearchBox.tsx
@@ -1,3 +1,4 @@
+import { SearchIcon } from '@heroicons/react/outline';
 import { Field, Form, Formik } from 'formik';
 import React from 'react';
 import { connectSearchBox } from 'react-instantsearch-dom';
@@ -16,25 +17,13 @@ function SearchBox({ refine }: Props) {
   return (
     <Formik onSubmit={handleSubmit} initialValues={{ query: '' }}>
       {() => (
-        <Form className="w-full flex md:ml-0">
+        <Form className="w-full flex">
           <label htmlFor="search-field" className="sr-only">
             Search
           </label>
           <div className="relative w-full text-gray-400 focus-within:text-gray-600">
             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-              <svg
-                className="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <SearchIcon className="h-5 w-5" />
             </div>
             <Field
               id="search-field"
