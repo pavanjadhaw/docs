c81e536de38a16bf30f1af8b73848def837a5bd3
Josue Montano <josuemontanoa@gmail.com>
2021-07-12 18:56:32 -0400

    Increase height of the search results list

--- pages/components/layout/SearchHits.tsx
+++ pages/components/layout/SearchHits.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 function SearchHits({ hits = [] }: Props) {
   return (
-    <ul className="max-h-64 overflow-y-scroll divide-y divide-gray-700 rounded-md">
+    <ul className="max-h-96 overflow-y-scroll divide-y divide-gray-700 rounded-md">
       {hits.map((hit, index) => (
         <li key={index}>
           <SearchHit hit={hit} />
