e4040be6ca420800d2b31e5f2134d00fb4930ddd
Josue Montano <josuemontanoa@gmail.com>
2021-07-12 20:15:40 -0400

    Show content of the hit

--- pages/components/layout/SearchHit.tsx
+++ pages/components/layout/SearchHit.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 interface Props {
@@ -5,20 +6,29 @@ interface Props {
     objectID: string;
     title: string;
     content: string;
+    slug: string;
     _highlightResult: {
       title: { value: string };
+      content: { value: string };
     };
   };
 }
 
 export default function SearchHit({ hit }: Props) {
-  const { _highlightResult } = hit;
+  const { _highlightResult, slug } = hit;
+
   return (
-    <article className="py-4 px-8 text-white hover:bg-gray-700">
-      <p
-        className="m-0"
-        dangerouslySetInnerHTML={{ __html: _highlightResult.title.value }}
-      />
-    </article>
+    <Link href={slug} passHref>
+      <article className="p-8 text-white hover:bg-gray-700 cursor-pointer">
+        <p
+          className="m-0"
+          dangerouslySetInnerHTML={{ __html: _highlightResult.title.value }}
+        />
+        <div
+          className="m-0 text-gray-400 truncate text-sm"
+          dangerouslySetInnerHTML={{ __html: _highlightResult.content.value }}
+        />
+      </article>
+    </Link>
   );
 }
