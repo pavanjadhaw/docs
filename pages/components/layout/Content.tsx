60b2911a5ff45ed4362029786829534ab720b839
Josue Montano <josuemontanoa@gmail.com>
2021-07-13 10:55:29 -0400

    Restore full width search box innmobile

--- pages/components/layout/Content.tsx
+++ pages/components/layout/Content.tsx
@@ -7,13 +7,13 @@ interface Props {
 
 export default function Content({ children }: Props) {
   return (
-    <div className="flex-1 mb-20 overflow-x-hidden w-full px-2 md:px-8">
-      <div className="flex flex-col md:px-8 xl:px-0">
+    <div className="flex-1 mb-20 overflow-x-hidden w-full md:px-8">
+      <div className="flex flex-col">
         <div className="md:mt-4 md:py-4 px-4 md:px-0 border-b border-gray-200">
           <SearchBar />
         </div>
-        <main className="flex-1 relative overflow-y-auto focus:outline-none">
-          <div className="p-4 w-full">{children}</div>
+        <main className="flex-1 relative overflow-y-auto focus:outline-none px-4 md:px-0">
+          <div className="w-full">{children}</div>
         </main>
       </div>
     </div>
