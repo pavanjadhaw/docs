faeb8b7c030860aa20afb0b73b4f950b68f575b7
Josue Montano <josuemontanoa@gmail.com>
2021-07-14 13:31:35 -0400

    Show the API reference menu only for the api reference pages

--- pages/components/menu/MenuItem.tsx
+++ pages/components/menu/MenuItem.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import { SitemapItem } from '../../../lib/sitemap';
 import OpenAPILinks from './OpenAPILinks';
@@ -5,9 +6,11 @@ import PageLink from './PageLink';
 import ParentMenuItem from './ParentMenuItem';
 
 export default function MenuItem(props: SitemapItem) {
+  const router = useRouter();
+
   if (props.children) return <ParentMenuItem {...props} />;
   if (props.to) {
-    if (props.to === '/rest-api/reference')
+    if (props.to === '/rest-api/reference' && router.asPath.startsWith('/rest-api'))
       return (
         <>
           <PageLink {...props} />
