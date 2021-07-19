4dd2dc020387d4b87b7468a4463e7f5add102059
Josue Montano <josuemontanoa@gmail.com>
2021-07-19 19:57:05 -0400

    Restore favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,14 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
-import IntercomScripts from "./components/scripts/IntercomScripts";
-import SegmentScripts from "./components/scripts/SegmentScripts";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import IntercomScripts from './components/scripts/IntercomScripts';
+import SegmentScripts from './components/scripts/SegmentScripts';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="shortcut icon" href="/docs/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
