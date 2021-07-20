f848da2f6b9766c2a01e1d8446ac726aff3e8837
Josue Montano <josuemontanoa@gmail.com>
2021-07-19 20:23:32 -0400

    Render breaklines in params' description

--- pages/components/openapi/Parameter.tsx
+++ pages/components/openapi/Parameter.tsx
@@ -13,7 +13,12 @@ export default function Parameter({ param }: Props) {
         {param.name}
         {param.required && <span className="text-red-500 mx-4 text-xs">required</span>}
       </p>
-      <p className="opacity-80 m-0">{param.description}</p>
+      <p
+        className="opacity-80 m-0"
+        dangerouslySetInnerHTML={{
+          __html: param.description?.replace(/\n/g, '<br/>') || '',
+        }}
+      />
       <p className="opacity-80 m-0 capitalize">
         {/* @ts-ignore */}
         {param.schema?.type}
