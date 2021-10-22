a052965c23e6c3665944f57ea506e425c8e6454e
Josue Montano <josuemontanoa@gmail.com>
2021-10-21 22:42:21 -0400

    style: implement new header design

--- pages/components/layout/Header.tsx
+++ pages/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { MenuIcon } from '@heroicons/react/outline';
 import React from 'react';
+import MagicBellLogo from '../../../public/magicbell.svg';
 
 interface Props {
   onToggleMenu: () => void;
@@ -12,36 +13,40 @@ export default function Header({ onToggleMenu }: Props) {
   };
 
   return (
-    <header className="z-20 flex-shrink-0 flex sticky top-0">
-      <div className="flex-1 hidden justify-between py-2 md:pr-8 md:flex max-w-screen-xl mx-auto">
-        <div className="flex-1 flex items-center">
-          <a href="https://magicbell.com">
-            <img src="/docs/MBUpdatedLogoWhite.svg" alt="MagicBell" style={{ height: '35px' }} />
-          </a>
-        </div>
-        <div className="ml-4 flex items-center md:ml-6 space-x-5 py-4">
-          <a href="https://magicbell.com/pricing">Pricing</a>
-          <a href="https://magicbell.com/docs">Docs</a>
-          <a href="https://magicbell.com/blog">Blog</a>
-          <a href="https://magicbell.com/about">About</a>
-          <a href="https://app.magicbell.com">Login</a>
-          <a className="secondary-button bg-transparent" href="https://magicbell.typeform.com/to/AgrVeIk5">
-            Schedule a Demo
-          </a>
-          <a className="main-button" href="https://app.magicbell.com">
-            Sign Up for Free
+    <header className="z-20 flex-shrink-0 flex sticky top-0 bg-white md:px-8 p-4">
+      <div className="flex-1 hidden justify-between md:flex max-w-screen-xl mx-auto">
+        <div className="flex-1 flex items-center space-x-10 py-4">
+          <div className="flex-1 flex space-x-24">
+            <a href="https://magicbell.com" aria-label="Go to homepage">
+              <MagicBellLogo
+                className="md:h-7 h-6 fill-current text-darkPurple"
+                role="img"
+                aria-label="MagicBell logo"
+                focusable="false"
+              />
+            </a>
+            <div className="flex-1 space-x-6">
+              <a href="https://magicbell.com/about">Notification Spec</a>
+              <a href="https://magicbell.com/docs">Docs</a>
+              <a href="https://magicbell.com/pricing">Pricing</a>
+            </div>
+          </div>
+          <a className="font-bold" href="https://app.magicbell.com">
+            Sign up
           </a>
         </div>
       </div>
       <div className="flex-1 flex items-center md:hidden">
         <a href="https://magicbell.com">
-          <img src="/docs/white-logo.png" alt="MagicBell" style={{ height: '40px' }} />
+          <MagicBellLogo
+            className="h-6 fill-current text-darkPurple"
+            role="img"
+            aria-label="MagicBell logo"
+            focusable="false"
+          />
         </a>
       </div>
-      <button
-        className="px-4 text-white focus:outline-none md:hidden h-20"
-        onClick={toggleMenu}
-      >
+      <button className="p-4 focus:outline-none md:hidden" onClick={toggleMenu}>
         <span className="sr-only">Open main menu</span>
         <MenuIcon className="h-6 w-6" aria-hidden="true" />
       </button>
