cd7d179794f3570d1e1ebb78e6cd60f269a90416
Josue Montano <josuemontanoa@gmail.com>
2022-01-28 11:48:41 -0400

    chore: sort imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 // @ts-ignore
 import { MDXProvider } from '@mdx-js/react';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import '../styles/material.css';
 import Callout from '../src/components/Callout';
 import Card from '../src/components/Card';
 import HighlightedCode from '../src/components/code/HighlightedCode';
@@ -10,6 +8,8 @@ import Grid from '../src/components/Grid';
 import NotificationInboxPreview from '../src/components/magicbell/NotificationInboxPreview';
 import Table from '../src/components/Table';
 import Tabs from '../src/components/tabs/Tabs';
+import '../styles/globals.css';
+import '../styles/material.css';
 
 const components = {
   code: HighlightedCode,
