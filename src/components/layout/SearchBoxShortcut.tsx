1ae29634497e25b081f2b62d659c74643ea4689c
Stephan Meijer <stephan.meijer@gmail.com>
2022-03-07 11:41:08 +0100

    feat: improve cmd+k shortcut alignment, and add ctrl+k alias (#71)

--- src/components/layout/SearchBoxShortcut.tsx
+++ src/components/layout/SearchBoxShortcut.tsx
@@ -3,24 +3,26 @@ import React, { useEffect } from 'react';
 
 export default function SearchBoxShortcut() {
   useEffect(() => {
-    hotkeys('command+K', function (event, handler) {
+    hotkeys('ctrl+k, command+K', function (event, handler) {
       event.preventDefault();
       document.getElementById('search-field')?.focus();
+      return false;
     });
+
     return () => {
-      hotkeys.unbind('command+K');
+      hotkeys.unbind('ctrl+k, command+K');
     };
   }, []);
 
   return (
     <div>
-      <span className="hidden sm:block text-gray-400 text-sm leading-5 py-0.5 px-1.5 border border-gray-300 rounded-md w-9">
+      <span className="hidden sm:flex items-center text-gray-400 text-sm leading-5 p-2 border border-gray-300 rounded-md">
         <span className="sr-only">Press </span>
         <kbd className="font-sans">
           <span title="Command">⌘</span>
         </kbd>
         <span className="sr-only"> and </span>
-        <kbd className="font-sans">K</kbd>
+        <kbd className="font-sans ml-1">K</kbd>
         <span className="sr-only"> to search</span>
       </span>
     </div>
